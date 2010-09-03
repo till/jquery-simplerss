1eaa6b544b482040e85bdc2077f92d293939cb77
Till Klampaeckel <till@php.net>
2010-09-03 18:43:45 +0200

    * fixes and refactorings (workz)

--- jquery.simplerss.js
+++ jquery.simplerss.js
@@ -1,4 +1,4 @@
-(function($){  
+(function($){
     jQuery.fn.extend({
         simplerss: function(options) {  
   
@@ -8,47 +8,38 @@
                 wrapper: 'li',
                 dataType: 'xml',
                 display: 2
-            }  
-            var options =  jQuery.extend(defaults, options);
+            }
+            var options = jQuery.extend(defaults, options);
   
             return this.each(function() { 
                 var o = options;
-                var c = $(this);
+                var c = jQuery(this);
 
                 jQuery.ajax({
                     url: o.url,
                     type: 'GET',
                     dataType: o.dataType,
                     error: function (xhr, status, e) {
-                        console.debug(xhr);
-                        console.debug(status);
-                        console.debug(e);
+                        console.debug('C: #%s, Error: %s, Feed: %s', $(c).attr('id'), e, o.url);
                     },
                     success: function(feed){
 
-                        var html  = '';
-                        var item  = null;
-
                         jQuery(feed).find('item').each(function(i){
 
-                            item = jQuery(this);
+                            var itemHtml = o.html.replace(/{title}/, jQuery(this).find('title').text());
+                            itemHtml = itemHtml.replace(/{text}/, jQuery(this).find('description').text());
+                            itemHtml = itemHtml.replace(/{link}/, jQuery(this).find('guid').text());
 
-                            var itemHtml = o.html.replace(/{title}/, jQuery(item).find('title').text());
-                            itemHtml = itemHtml.replace(/{text}/, jQuery(item).find('description').text());
-                            itemHtml = itemHtml.replace(/{link}/, jQuery(item).find('guid').text());
-
-                            html += itemHtml;
+                            jQuery(c).append(jQuery('<' + o.wrapper + '>').append(itemHtml));
 
                             if (i == o.display) {
-                                break
+                                return;
                             }
 
                         });
-
-                        jQuery(c).append(html);
                     }
                 });
-            }
+            });
         }
     });
 })(jQuery);
